8d340f7a8cf6449de3063b6fa1c712eadf670e57
Kevin Steven <kperez@skgtecnologia.com>
2024-07-08 13:50:17 -0500

    add nosotros module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NOVA HEALTH INC';
+  title = 'Cocreamos INC';
 }
--- src/app/shared/header/header/header.component.ts
+++ src/app/shared/header/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private el: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let images = this.el.nativeElement.querySelectorAll('img');
+    if (window.pageYOffset > 100) { // Ajusta este valor a la cantidad de scroll que quieras antes de que las imágenes se hagan más pequeñas
+      images.forEach((img: HTMLImageElement) => { // Explicitly type 'img' as 'HTMLImageElement'
+        img.classList.add('smaller');
+      });
+    } else {
+      images.forEach((img: HTMLImageElement) => { // Explicitly type 'img' as 'HTMLImageElement'
+        img.classList.remove('smaller');
+      });
+    }
+  }
 
 }
