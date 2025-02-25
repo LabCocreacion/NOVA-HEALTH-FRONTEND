59ff9695249e957595dedff8c19d5d65e9a02ac8
KevinSteven <kperez@skgtecnologia.com>
2025-02-25 15:19:30 -0500

    add instituciones map component

---
+++ src/app/pages/projects/tmama/list-instituciones-map/list-instituciones-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-instituciones-map',
+  imports: [],
+  templateUrl: './list-instituciones-map.component.html',
+  styleUrl: './list-instituciones-map.component.css'
+})
+export class ListInstitucionesMapComponent {
+
+}
