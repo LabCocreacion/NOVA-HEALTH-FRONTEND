d6f8341e365a65ad0518c1c6b6be53526f38165e
KevinSteven <kperez@skgtecnologia.com>
2025-03-06 21:17:00 -0500

    update bg

--- src/app/pages/projects/dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/projects/dashboard/dashboard/dashboard.component.ts
@@ -10,11 +10,32 @@ import { LoginService } from 'src/app/core/services/login/login.service';
 export class DashboardComponent {
 
   userInfo: any
+  backgroundClass: string = '';
 
   constructor(public loginService: LoginService) { }
 
   ngOnInit() {
     this.userInfo = this.loginService.getUserInfo()
+  }
+
+  getBackgroundImage(): string {
+    if (this.userInfo.userProject === 'Phantom') {
+      return "url('/assets/img/bg.jpg')";
+    } else if (this.userInfo.userProject === 'Tamización Mama') {
+      return "url('/assets/img/bg_mama.jpg')";
+    } else {
+      return "none";
+    }
+  }
+
+  getBackgroundColor(): string {
+    if (this.userInfo.userProject === 'Phantom') {
+      return "rgba(0, 0, 0, 0.342)";
+    } else if (this.userInfo.userProject === 'Tamización Mama') {
+      return "rgba(0, 0, 0, 0.21)";
+    } else {
+      return "#fff";
+    }
   }
 
 }
--- src/app/shared/header/header-login/header-login.component.ts
+++ src/app/shared/header/header-login/header-login.component.ts
@@ -10,17 +10,39 @@ import { Router } from '@angular/router';
 })
 export class HeaderLoginComponent {
   userName: string | null = null;
+  userInfo: any
 
   constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
     this.userName = this.loginService.getUserName();
     console.log('User Name:', this.userName); // Debugging line
+    this.userInfo = this.loginService.getUserInfo()
   }
 
   logout(): void {
     this.loginService.logout();
     this.router.navigate(['/index']);
+  }
+
+  getBackgroundColor(): string {
+    if (this.userInfo.userProject === 'Phantom') {
+      return '#1B3A4B';
+    } else if (this.userInfo.userProject === 'Tamización Mama') {
+      return '#FFFFFF';
+    } else {
+      return '#1B3A4B'; // Default color
+    }
+  }
+
+  getBackgroundColorButton(): string {
+    if (this.userInfo.userProject === 'Phantom') {
+      return '#5DA3A4';
+    } else if (this.userInfo.userProject === 'Tamización Mama') {
+      return '#C8A2C8';
+    } else {
+      return '#5DA3A4'; // Default color
+    }
   }
 
 }
